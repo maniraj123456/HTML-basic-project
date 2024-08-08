6e8ce9183af718342e322c13d8c1998734767128
maniraj <lingalamaniraj123@gmail.com>
2024-08-08 19:05:13 +0530

    storing the quiz in local storage

--- homePage.js
+++ homePage.js
@@ -4,22 +4,49 @@ function submit() {
   const message = document.getElementById("error-message");
 
   for (let i = 0; i < questionsText.length; i++) {
-    if (
-      questionsText[i].ariaValueText == "" ||
-      questionsText[i].ariaValueText == null
-    ) {
+    if (questionsText[i].value == "" || questionsText[i].value == null) {
       console.log(message.style.display);
       message.style.display = "inline";
-      break;
+      return;
     }
-    if (
-      solutionsText[i].ariaValueText == "" ||
-      solutionsText[i].ariaValueText == null
-    ) {
+    if (solutionsText[i].value == "" || solutionsText[i].value == null) {
       message.style.display = "inline";
-      break;
+      return;
     }
   }
+  let questions = [];
+  let solutions = [];
+
+  for (let i = 0; i < questionsText.length; i++)
+    questions.push(questionsText[i].value);
+  for (let i = 0; i < solutionsText.length; i++)
+    solutions.push(solutionsText[i].value);
+
+  message.style.display = "none";
+
+  let quiz = {
+    questions: questions,
+    solutions: solutions,
+    key: Math.floor(Math.random() * 100),
+  };
+  let quizValue = JSON.stringify(quiz);
+  if (localStorage.getItem("quizzes") == null || localStorage.getItem("quizzes").length == 0) 
+  {
+    let quizes = [];
+    quizes.push(quizValue);
+    localStorage.setItem("quizzes", JSON.stringify(quizes));
+    return;
+  } 
+  else 
+  {
+    let existingQuizzes = localStorage.getItem("quizzes");
+    let quizzes = JSON.parse(existingQuizzes);
+    while(quizzes.find(element => element.key === quiz.key))
+      quiz.key = Math.floor(Math.random() * 100);
+    quizzes.push(quizValue);
+    localStorage.setItem("quizzes", JSON.stringify(quizzes));
+  }
+
 }
 
 function clear() {
--- image-component.js
+++
@@ -1,8 +0,0 @@
-class MyElement extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({ mode: "open" });
-    this.shadowRoot.innerHTML = `<p>my custom element</p>`;
-  }
-}
-customElements.define("my-element", MyElement);
